93aa2eb56bac5135ad6e8908f02515e7022b7f20
shoyama1010 <kochisk8@gmail.com>
2026-02-23 21:13:38 +0900

    API連携更新

--- src/config/cors.php
+++ src/config/cors.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
