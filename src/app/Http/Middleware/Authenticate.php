2e20d9cc49efe43f373fbb5e3cab2186292124f0
shoyama1010 <kochisk8@gmail.com>
2026-07-20 12:32:03 +0900

    Return 401 for unauthenticated API requests

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -7,14 +7,18 @@ use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
-    
-    protected function redirectTo($request): ?string
-    {
-        if ($request->expectsJson() || $request->is('api/*')) {
-            return null;
-        }
 
-        return route('login');
+    protected function redirectTo($request)
+    {
+        return null;
     }
+
+    // protected function redirectTo($request): ?string
+    // {
+    //     if ($request->expectsJson() || $request->is('api/*')) {
+    //         return null;
+    //     }
+    //     return route('login');
+    // }
 
 }
